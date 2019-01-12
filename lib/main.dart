6e4cf5b7b0df4b3e617c8ffd73409709b66f5135
Raghav Garg <42090624+Raghav117@users.noreply.github.com>
2019-01-12 21:49:18 +0530

    Update main.dart
    No method to run the application (main).

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(TabLayoutDemo());
+
 class TabLayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
